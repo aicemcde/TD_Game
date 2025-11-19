b937d681b1342969b558405c900fa025a56b9dd8
Ryosuke Tsukuda <ryosuke.tsukuda1019@gmail.com>
2025-11-19 19:14:48 +0900

    MC　OK Next TileMap

---
+++ TD_Game/MoveComponent.cpp
@@ -0,0 +1,25 @@
+#include "MoveComponent.h"
+#include "Actor.h"
+
+MoveComponent::MoveComponent(class Actor* owner, int updateOrder)
+	:Component(owner, updateOrder)
+	,mForwardSpeed(0.0f)
+	,mAngularSpeed(0.0f)
+{
+}
+
+void MoveComponent::Update(float delta)
+{
+	if (Math::NearZero(mAngularSpeed))
+	{
+		float rot = mOwner->GetRot();
+		rot += mAngularSpeed * delta;
+		mOwner->SetRot(rot);
+	}
+	if (Math::NearZero(mForwardSpeed))
+	{
+		Vector2 pos = mOwner->GetPos();
+		pos += mOwner->GetForward() * mForwardSpeed * delta;
+		mOwner->SetPos(pos);
+	}
+}
---
+++ TD_Game/MoveComponent.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Component.h"
+
+class MoveComponent : public Component
+{
+public:
+	MoveComponent(class Actor* owner, int updateOrder = 10);
+
+	void Update(float delta);
+
+	float GetForwardSpeed() const { return mForwardSpeed; }
+	float GetAngularSpeed() const { return mAngularSpeed; }
+	void SetForwardSpeed(float speed) { mForwardSpeed = speed; }
+	void SetAngularSpeed(float speed) { mAngularSpeed = speed; }
+private:
+	float mForwardSpeed;
+	float mAngularSpeed;
+};
